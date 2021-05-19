ddb5989a33796cd17ddca5289e6c67b3885956de
majawieland <73876215+majawieland@users.noreply.github.com>
2021-05-19 14:12:52 +0200

    tilføjelse af booking funktion + javascript

---
+++ eksamen/js/prisudregner.js
@@ -0,0 +1,17 @@
+const prisvoksen = 130;
+const prisbarn = 75;
+const prisinfant = 0;
+const prispensionist = 110;
+
+function samletpris(){
+ 
+    
+    var res = (prisvoksen * parseInt(document.getElementById("Voksne").value)) + (prisbarn * parseInt(document.getElementById("Barn").value)) + (prisinfant * parseInt(document.getElementById("Infant").value)) + (prispensionist * parseInt(document.getElementById("pensionist").value));
+   
+    document.getElementById("result").innerHTML=res;
+    
+    
+    
+
+}
+

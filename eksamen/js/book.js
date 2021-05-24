db6acca0334508f17aff5e7717da69a0e9bdeda8
Mille Iversen <milleiversen13a@gmail.com>
2021-05-24 14:52:52 +0200

    book

---
+++ eksamen/js/book.js
@@ -0,0 +1,29 @@
+const BOOKKNAP = document.getElementById("bookknap");
+
+BOOKKNAP.addEventListener("click", function () {
+    sessionStorage.setItem("fornavn", document.getElementById("fornavn").value);
+    sessionStorage.setItem("efternavn", document.getElementById("efternavn").value);
+    sessionStorage.setItem("email", document.getElementById("email").value);
+    sessionStorage.setItem("adresse", document.getElementById("adresse").value);
+    sessionStorage.setItem("postnummer", document.getElementById("postnummer").value);
+})
+
+
+
+fetch("https://api.dataforsyningen.dk/postnumre/")
+    .then(function (data) {
+        return data.json();
+    })
+
+    .then(function (post) {
+        const POSTNUMRELISTE = document.getElementById("postnummer");
+        for (let i = 0; i < post.length; i++) {
+            POSTNUMRELISTE.insertAdjacentHTML("beforeend", "<option>" + post[i].nr + " " + post[i].navn + "</option>");
+        }
+    })
+    .catch(function (error) {
+        const POSTNUMMERINPUT = document.getElementById("postnr");
+       POSTNUMMERINPUT.value = "Ingen postnumre er at vælge.";
+    })
+
+

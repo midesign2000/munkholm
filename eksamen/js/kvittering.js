13e28b1e9d02094f3c45dedbc40b60f0b4e4b0b6
majawieland <73876215+majawieland@users.noreply.github.com>
2021-05-24 18:12:49 +0200

    Bookingside on it's way

---
+++ eksamen/js/kvittering.js
@@ -0,0 +1,47 @@
+window.onload = function() {
+    takTekst();
+  kvitteringsTekst();
+}
+
+
+
+
+function takTekst(){
+    document.getElementById("takTekst").innerHTML = "Tak for din bestilling " + sessionStorage.getItem("fornavn") + " vi glæder os til at se dig den " + sessionStorage.getItem("ValgAfDato");
+}
+
+function kvitteringsTekst(){
+    
+    document.getElementById("fornavn").innerHTML = "Navn: " + sessionStorage.getItem("fornavn")+ " " + sessionStorage.getItem("efternavn");
+    document.getElementById("email").innerHTML = "Email: " + sessionStorage.getItem("email");
+    document.getElementById("adresse").innerHTML = "Adresse: " + sessionStorage.getItem("adresse") + " " + sessionStorage.getItem("postnummer");
+
+    document.getElementById("pris").innerHTML = "Pris " + sessionStorage.getItem("SamletPris") + " kr";
+    
+    
+    
+    /*Der er lavet en if sætning for at lave et tjek på om persontypen er valgt, og for i kvitteringen at vise brugeren hvad de har købt og betalt. Elementerne der ikke er valgt bliver dermed ikke vist */
+    
+    let voksne = sessionStorage.getItem("Voksen");
+    if(voksne > 0){
+        document.getElementById("antalVoksne").innerHTML = "Antal voksne " + voksne;
+    }
+    
+    let barn = sessionStorage.getItem("Barn");
+    if(barn > 0){
+        document.getElementById("antalBarn").innerHTML = "Antal børn " + barn;
+    }
+    
+    let infant = sessionStorage.getItem("Infant");
+    if(infant > 0){
+        document.getElementById("antalBarnU2").innerHTML = "Antal børn under 2 år " + infant;
+    }
+
+    let pensionist = sessionStorage.getItem("Pensionist");
+    if(pensionist > 0){
+        document.getElementById("pensionister").innerHTML = "Antal pensionister " + pensionist;
+    }
+
+    document.getElementById("dato").innerHTML = "Din valgte dato " + sessionStorage.getItem("ValgAfDato");
+
+}
--- eksamen/js/prisudregner.js
+++ eksamen/js/prisudregner.js
@@ -23,29 +23,32 @@ function samletpris(){
 
     let res = (PRISVOKSEN * parseInt(document.getElementById("Voksne").value)) + (PRISBARN * parseInt(document.getElementById("Barn").value)) + (PRISINFANT * parseInt(document.getElementById("Infant").value)) + (PRISPENSIONIST * parseInt(document.getElementById("pensionist").value));
    
+    
     document.getElementById("result").innerHTML=res;
-
+    document.getElementById("result").value = res;
 }
 
-/* setItem er en funktion og den bliver kaldt med to parametre som er det man kalder et key/value pair - i det her tilfælde er vores parameter "Voksen" & værdien af dropdown elementer "voksne" */
+/* setItem er en funktion og den bliver kaldt med to parametre som er det man kalder et key/value pair - i det her tilfælde er vores parameter "Voksen" & værdien af dropdown elementer "voksne" 
+
+sessionStorage bliver oprettet når en fane oprettes, og holder keyvalue pass og fungere som et opslagsværk af de informationer vi har kommet i dem - de eksistere ind til fanen lukkes */
+
+/*If sætning - hvis antallet af voksne eller pensionister er større end 0 = går videre. Ellers informer om at der skal vælges en voksen */
 
 function gemValg(){
-    window.sessionStorage.setItem("Voksen",document.getElementById("Voksne").value);
+    if(document.getElementById("Voksne").value > 0 || document.getElementById("pensionist").value > 0){
+        sessionStorage.setItem("Voksen",document.getElementById("Voksne").value);
+        sessionStorage.setItem("Barn",document.getElementById("Barn").value);
+        sessionStorage.setItem("Infant", document.getElementById("Infant").value);
+        sessionStorage.setItem("Pensionist", document.getElementById("pensionist").value);
+        sessionStorage.setItem("SamletPris", document.getElementById("result").value);
+        sessionStorage.setItem("ValgAfDato", document.getElementById("valgafdato").value);
+
+
+        location.href="Book.html";
+    }
+    else{
+        document.getElementById("fejlbeskred").innerHTML = "Der skal være en voksen med."
+    }
     
-    sessionStorage.setItem("Barn",document.getElementById("Barn").value);
-    
-    sessionStorage.setItem("Infant", document.getElementById("Infant").value);
-    
-    
-    
-    sessionStorage.setItem("Pensionist", document.getElementById("pensionist").value);
-    
-    
- window.sessionStorage.setItem("SamletPris", document.getElementById("result").value);
-    
-    
- sessionStorage.setItem("ValgAfDato", document.getElementById("valgafdato").value);
-    
-    location.href="Book.html";
 }
 
